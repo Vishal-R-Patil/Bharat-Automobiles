ca4a1b3f50c6e732db6cb3965b711af291f3d6e2
Vishal <vispat26@gmail.com>
2026-04-17 23:28:13 +0530

    ssl warning resolved

--- server/config/database.js
+++ server/config/database.js
@@ -1,15 +1,19 @@
-const mysql= require('mysql2/promise');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+    uri: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false }
+});
 
 // Test the connection
 pool.getConnection()
-    .then(connection => {
+    .then(connection =>
+    {
         console.log("✅ Successfully connected to the Bharat Automobiles database!");
         connection.release();
     })
-    .catch(err => {
+    .catch(err =>
+    {
         console.error("❌ Database connection failed: ", err.message);
     });
 
